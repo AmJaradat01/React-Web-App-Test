e09e6e5d101a3cf7396f2b8848230dba138f4740
Ali Jaradat <ali.jaradat@shahid.net>
2023-11-17 00:14:53 +0300

    - add dark/light theme toggle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import BlogPage from './pages/BlogPage';
-import styled from 'styled-components';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
+import { lightTheme, darkTheme } from './style/theme';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: ${(props) => props.theme.body};
+    color: ${(props) => props.theme.text};
+  }
+`;
 
 const Header = styled.header`
   display: flex;
@@ -27,26 +35,38 @@ const Nav = styled.nav`
 `;
 
 function App() {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+  const themeToggler = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+
   return (
-    <Router>
-      <div>
-        <Header>
-          <div>Great Irbid</div>
-          <Nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/blog">Blog</Link>
-          </Nav>
-        </Header>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <button onClick={themeToggler}>Toggle Theme</button>
+      <Router>
+        <div>
+          <Header>
+            <div>Great Irbid</div>
+            <Nav>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/contact">Contact</Link>
+              <Link to="/blog">Blog</Link>
+            </Nav>
+          </Header>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/blog" element={<BlogPage />} />
           </Routes>
-      </div>
-    </Router>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
