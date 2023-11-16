8fce565f32657d5477da0a9d45ceb3718306a3c1
Ali Jaradat <ali.jaradat@shahid.net>
2023-11-17 00:27:18 +0300

    add style controller for Home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,20 +4,29 @@ import styled from 'styled-components';
 const Card = styled.div`
   margin: 2rem;
   padding: 1rem;
-  background-color: #f7f7f7;
+  background-color: ${(props) => props.theme.body};
+  color: ${(props) => props.theme.text};
   border-radius: 8px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const StyledHeading = styled.h2`
+  color: ${(props) => props.theme.text};
+`;
+
+const StyledParagraph = styled.p`
+  color: ${(props) => props.theme.text};
 `;
 
 const HomePage = () => {
   return (
     <div>
-      <h2>Welcome</h2>
+      <StyledHeading>Welcome</StyledHeading>
       <Card>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa</p>
-        <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus</p>
-        <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum</p>
-        <p>**All above are auto generated text</p>
+        <StyledParagraph>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa</StyledParagraph>
+        <StyledParagraph>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus</StyledParagraph>
+        <StyledParagraph>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum</StyledParagraph>
+        <StyledParagraph>**All above are auto generated text</StyledParagraph>
       </Card>
     </div>
   );
